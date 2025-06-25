a1400d820694958d9b37537156eaeec8f9993275
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-25 13:19:07 +0600

    feat: create auth and role middleware

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,31 @@
+import User from "../models/User.js";
+import { decodeToken, getTokenFromHeader } from "../utils/utils.js";
+
+export async function authMiddleware(req, res, next) {
+  try {
+    // Get the token from the request header
+    const token = getTokenFromHeader(req);
+
+    // Check if the token is present
+    if (!token) return res.status(401).json({ message: "Token missing" });
+
+    // Decode the token to get the user ID
+    const { id } = decodeToken(token);
+
+    // Check if the user ID is present
+    if (!id) return res.status(401).json({ message: "Invalid token" });
+
+    // Find the user by ID
+    const user = await User.findById(id).select("-password");
+
+    // Check if the user exists
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    // Attach the user object to the request object
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+}
---
+++ backend/middlewares/roleMiddleware.js
@@ -0,0 +1,82 @@
+// Define allowed roles for each path
+const ROLE = {
+  "/admin": ["admin:get", "admin:post", "admin:put", "admin:delete"],
+  "/profile": [
+    "user:get",
+    "user:post",
+    "user:put",
+    "user:delete",
+    "admin:get",
+    "admin:post",
+    "admin:put",
+    "admin:delete",
+  ],
+  "/products": [
+    "public:get",
+    "user:get",
+    "admin:get",
+    "admin:post",
+    "admin:put",
+    "admin:delete",
+  ],
+  "/products/:id": [
+    "public:get",
+    "user:get",
+    "admin:get",
+    "admin:post",
+    "admin:put",
+    "admin:delete",
+  ],
+  "/cart": [
+    "user:get",
+    "user:post",
+    "user:put",
+    "user:delete",
+    "admin:get",
+    "admin:post",
+    "admin:put",
+    "admin:delete",
+  ],
+};
+
+// Function to validate roles
+const validateRole = (path, role, method) => {
+  const allowedRoles = ROLE[path].includes(`${role}:${method}`);
+  return allowedRoles;
+};
+
+// Function to validate path
+const validatePath = (path) => {
+  return Object.keys(ROLE).find((url) => {
+    if (url === "/products/:id") {
+      const match = /^\/products\/\w+$/.test(path);
+      return match ? url : false;
+    } else {
+      return path === url ? url : false;
+    }
+  });
+};
+
+// Role middleware
+export const roleMiddleWare = async (req, res, next) => {
+  try {
+    const path = validatePath(req.path);
+    if (!path) return res.status(404).json({ message: "Path not found" });
+
+    const role = req?.user?.role || "public";
+
+    // Check if the path has defined roles
+    const allowedRoles = validateRole(path, role, req.method.toLowerCase());
+
+    // Verify if the user's role is allowed for the path
+    if (!allowedRoles) {
+      return res.status(403).json({ message: "Unauthorized access" });
+    }
+
+    next();
+  } catch (error) {
+    // Handle errors and respond with a server error message
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
