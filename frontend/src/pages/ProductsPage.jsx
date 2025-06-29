9ac86073515434014f9509183aad88dcc72b0a35
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-29 19:05:03 +0600

    Update ProductsPage

--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -1,4 +1,39 @@
+import { useEffect, useState } from "react";
+import { api } from "../api/axios";
+import Product from "../components/Product";
+
 function ProductsPage() {
-  return <div className="page">ProductsPage</div>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const res = await api.get("/products");
+        setProducts(res.data.products || []);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    getProducts();
+  }, []);
+  return (
+    <div className="page">
+      <h1 className="text-4xl font-bold mt-5">All products</h1>
+      {loading ? (
+        <div>Loading products....</div>
+      ) : products.length ? (
+        <div className="grid grid-cols-3 gap-5 my-10">
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      ) : (
+        <div>No products found</div>
+      )}
+    </div>
+  );
 }
 export default ProductsPage;
