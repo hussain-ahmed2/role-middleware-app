5113b7a0b891549682da2b36a11180ee91586863
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-29 19:09:52 +0600

    Add CartItem component

---
+++ frontend/src/components/CartItem.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import IncrementAndDecrementQuantityButtons from "./IncrementAndDecrementQuantityButtons";
+import { Trash2 } from "lucide-react";
+
+function CartItem({ item }) {
+  const [quantity, setQuantity] = useState(item.quantity);
+  const { deleteFromCart } = useAuth();
+
+  return (
+    <div
+      className="grid grid-cols-5 gap-5 p-3 border-b border-neutral-600 items-center"
+      key={item.product._id}
+    >
+      <p>{item.product.name}</p>
+      <p>${item.product.price}</p>
+      <p>{quantity}</p>
+      <p>${(item.product.price * item.quantity).toFixed(2)}</p>
+      <div className="flex gap-2 items-center">
+        <IncrementAndDecrementQuantityButtons
+          id={item.product._id}
+          quantity={quantity}
+          setQuantity={setQuantity}
+        />
+        <button
+          onClick={() => deleteFromCart(item.product._id)}
+          className="p-2 rounded-lg bg-neutral-800 hover:bg-rose-600 transition duration-300 ease-in-out"
+        >
+          <Trash2 />
+        </button>
+      </div>
+    </div>
+  );
+}
+export default CartItem;
