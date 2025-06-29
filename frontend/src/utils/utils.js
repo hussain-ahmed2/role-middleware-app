177c301882b3999bcff72987657060bcb9573e4c
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-29 19:05:35 +0600

    Update utils

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -9,3 +9,12 @@ export async function getAvatars() {
     return [];
   }
 }
+
+export const cb64f = async (base64) => {
+  const res = await fetch(base64);
+  const blob = await res.blob();
+  const filename = base64?.split("/")?.pop() || "avatar.jpg";
+  return new File([blob], filename, {
+    type: blob.type,
+  });
+};
