e771bb8d136d32b6a15be57a52c092a166e9c2dc
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-29 19:10:01 +0600

    Add IncrementAndDecrementQuantityButtons component

---
+++ frontend/src/components/IncrementAndDecrementQuantityButtons.jsx
@@ -0,0 +1,31 @@
+import { Minus, Plus } from "lucide-react";
+import { useAuth } from "../hooks/useAuth";
+import { useEffect } from "react";
+
+function IncrementAndDecrementQuantityButtons({ id, quantity, setQuantity }) {
+  const { updateCart } = useAuth();
+
+  useEffect(() => {
+    const timer = setTimeout(() => updateCart(id, quantity), 1000);
+    return () => clearTimeout(timer);
+  }, [quantity]);
+
+  return (
+    <div className="flex gap-2 items-center">
+      <button
+        disabled={quantity <= 1}
+        className="p-2 rounded-lg bg-neutral-800 hover:bg-rose-600 transition duration-300 ease-in-out disabled:opacity-50 disabled:bg-neutral-800 size-10"
+        onClick={() => setQuantity((prev) => prev - 1)}
+      >
+        <Minus />
+      </button>
+      <button
+        className="p-2 rounded-lg bg-neutral-800 hover:bg-green-600 transition duration-300 ease-in-out size-10"
+        onClick={() => setQuantity((prev) => prev + 1)}
+      >
+        <Plus />
+      </button>
+    </div>
+  );
+}
+export default IncrementAndDecrementQuantityButtons;
