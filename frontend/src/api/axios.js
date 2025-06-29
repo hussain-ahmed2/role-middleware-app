e2b2fc387a404434ffefc3f52419b5309153949a
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-29 19:03:52 +0600

    Update axios configuration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  headers: { "Content-Type": "application/json" },
 });
