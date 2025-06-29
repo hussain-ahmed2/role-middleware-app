f5aff79dfe7d42e70d3349019146a3daaf816238
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-06-29 19:03:04 +0600

    Update backend zod schemas

--- backend/validation/zod-schemas.js
+++ backend/validation/zod-schemas.js
@@ -9,7 +9,7 @@ export const loginSchema = z.object({
 });
 
 export const registerSchema = z.object({
-  defaultAvatar: z.string("Default avatar is required").trim(),
+  avatar: z.instanceof(File).optional(),
   name: z
     .string("Name is required")
     .trim()
@@ -21,13 +21,34 @@ export const registerSchema = z.object({
     .min(8, "Password must be at least 8 characters long"),
 });
 
-export const credentialsSchema = z.object({
-  name: z
-    .string("Name is required")
-    .trim()
-    .min(3, "Name must be at least 3 characters long"),
-  email: z.string("Email is required").email("Invalid email format").trim(),
-});
+export const credentialsSchema = z
+  .object({
+    name: z
+      .string()
+      .trim()
+      .nonempty("Name is required")
+      .min(3, "Name must be at least 3 characters long"),
+    email: z
+      .string()
+      .trim()
+      .nonempty("Email is required")
+      .email("Invalid email format"),
+    currentPassword: z.string().trim().optional(),
+    newPassword: z.string().trim().optional(),
+    avatar: z.instanceof(File).optional(),
+  })
+  .refine(
+    (data) => {
+      if (data.newPassword && !data.currentPassword) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: "Current password is required when setting a new password",
+      path: ["currentPassword"],
+    }
+  );
 
 export const passwordSchema = z.object({
   oldPassword: z
